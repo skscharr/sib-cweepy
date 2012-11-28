0ce38f9888458a16cec8853712aaa90bc0e0c0ce
Simon Højberg <r.hackr@gmail.com>
2012-11-28 18:50:16 -0500

    add help in file and readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-cweepy-01
+Boston Startup School execise "Cweepy"
 =========
+
+1. Add an event listener for the button 
+
+2. render a form inside the .header from that event.
+   wrap the form with in an .add-cweep-form div to get
+   the styles.
+
+   The form should have an <input class='message'/>
+   and a <a class='button'>Post'</a>
+
+3. Add a 'cweep' to the list when the form is submitted
--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
 // Start the application when the dom ready event has fired 
 $(document).ready(function () {
 
-  // add your 'new cweep' code here
+  /**
+ 
+  1. Add an event listener for the button 
+
+  2. render a form inside the .header from that event.
+     wrap the form with in an .add-cweep-form div to get
+     the styles.
+
+     The form should have an <input class='message'/>
+     and a <a class='button'>Post'</a>
+
+  3. Add a 'cweep' to the list when the form is submitted
+   
+  **/
 
 });
